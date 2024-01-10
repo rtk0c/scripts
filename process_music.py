9649f37f95965b0ec9d0ac7640ce86dc95114c43
rtk0c <mail.tianyig@gmail.com>
2024-01-10 11:35:49 -0800

    Migrate to properly typed data structures

--- process_music.py
+++ process_music.py
@@ -5,7 +5,8 @@ import shutil
 import itertools
 import argparse
 import json
-from typing import Optional, Tuple
+from typing import Optional
+from dataclasses import dataclass, field
 
 import ffmpeg
 import music_tag
@@ -14,31 +15,60 @@ from yt_dlp import YoutubeDL
 # Local script files
 import my_util as MU
 
+@dataclass
+class TagOp():
+	name: str = ''
+	value: str = ''
+
+@dataclass
+class Chunk():
+	begin_time: float = 0.0
+	end_time: float | str = ''
+	chunk_name: str = ''
+	tag_ops: list[TagOp] = field(default_factory=list)
+	out_basename: Optional[str] = None
+	out_filepath: Optional[str] = None
+
+@dataclass
+class Job():
+	url: str = ''
+	chunk_by_chapter: bool = False
+	prefix: Optional[str] = None
+	suffix: Optional[str] = None
+	video_id: str = ''
+	video_info: dict = field(default_factory=dict)
+	tag_ops: list[TagOp] = field(default_factory=list)
+	chunks: list[Chunk] = field(default_factory=list)
+
+@dataclass
+class InputStruct():
+	job_list: list[Job] = field(default_factory=list)
+
 def use_pattern(pattern, txt, idx):
 	return pattern.format(
 		index = idx + 1,
 		index_roman = MU.int_to_roman_unicode(idx + 1))
 
 # Pass 1
-def pass_postprocess_info(input_struct):
-	for entry in input_struct['chunk_list']:
-		url = entry['url']
-		chunks = entry['chunks']
+def pass_postprocess_info(input_struct: InputStruct):
+	print(input_struct)
+	for job in input_struct.job_list:
+		chunks = job.chunks
 		for i in range(len(chunks)):
 			curr_chunk = chunks[i]
 			next_chunk = chunks[i + 1] if i + 1 < len(chunks) else None
 
-			if curr_chunk['end_time'] == 'NEXTCHUNK':
-				if next_chunk:
-					curr_chunk['end_time'] = next_chunk['begin_time']
+			if curr_chunk.end_time == 'NEXTCHUNK':
+				if next_chunk is not None:
+					curr_chunk.end_time = next_chunk.begin_time
 				else:
-					curr_chunk['end_time'] = 'VIDEOLENGTH'
+					curr_chunk.end_time = 'VIDEOLENGTH'
 
 # Pass 2
-def pass_prepare_yt_dlp(input_struct):
+def pass_prepare_yt_dlp(input_struct: InputStruct):
 	# TODO maybe add a cache timeout for video info?
-	for entry in input_struct['chunk_list']:
-		ytb_id = MU.get_id_from_ytb_url(entry['url'])
+	for job in input_struct.job_list:
+		ytb_id = MU.get_id_from_ytb_url(job.url)
 		os.makedirs(ytb_id, exist_ok=True)
 
 		info_file_path = os.path.join(args.work_dir, ytb_id, '$info.json')
@@ -48,7 +78,7 @@ def pass_prepare_yt_dlp(input_struct):
 				info = json.load(info_file)
 		except:
 			print('-- $info.json not found, downloading')
-			info = ydl.extract_info(entry['url'], download=False)
+			info = ydl.extract_info(job.url, download=False)
 			with open(info_file_path, 'w') as info_file:
 				print('-- Saving $info.json')
 				info_file.write(json.dumps(info))
@@ -60,42 +90,37 @@ def pass_prepare_yt_dlp(input_struct):
 		except:
 			pass
 
-		entry['video_id'] = ytb_id
-		entry['video_info'] = info
+		job.video_id = ytb_id
+		job.video_info = info
 
 # Pass 3
-def pass_video_dependent_info(input_struct):
-	for entry in input_struct['chunk_list']:
-		url = entry['url']
-		video_info = entry['video_info']
-		chunks = entry['chunks']
-		use_suffix = entry.get('suffix')
-		use_prefix = entry.get('prefix')
+def pass_video_dependent_info(input_struct: InputStruct):
+	for job in input_struct.job_list:
+		chunks = job.chunks
 
 		# Section: generating new chunks
 		# (there should be no such thing happening later as we'll proceed to post process chunk info)
-		video_chapters = video_info.get('chapters')
-		if entry.get('chunk_by_chapter') and video_chapters:
+		video_chapters = job.video_info.get('chapters')
+		if job.chunk_by_chapter and video_chapters:
 			for chap in video_chapters:
-				# TODO this should use logic in commit_entry()
-				chunks.append({
-					'begin_time': chap['start_time'],
-					'end_time': chap['end_time'],
-					'out_basename': MU.format_filename(chap['title'])
-				})
+				chunks.append(Chunk(
+					begin_time=chap['start_time'],
+					end_time=chap['end_time'],
+					chunk_name=chap['title'],
+					out_basename=MU.format_filename(chap['title'])))
 
 		for i, chunk in enumerate(chunks):
 			# NOTE: we leave 'end_time' special value VIDEOLENGTH as is because it's easy to compute, and knowing it is required in pass_split_chunks()
 			# output FileName
 			fn = ""
-			if use_prefix:
-				fn = use_pattern(use_prefix, fn, i)
-			fn += chunk['chunk_name']
-			if use_suffix:
-				fn += use_pattern(use_suffix, fn, i)
-			chunk['out_basename'] = fn
+			if job.prefix:
+				fn = use_pattern(job.prefix, fn, i)
+			fn += chunk.chunk_name
+			if job.suffix:
+				fn += use_pattern(job.suffix, fn, i)
+			chunk.out_basename = fn
 
-def obtain_video_with_yt_dlp(video_url: str) -> Tuple[str, str]:
+def obtain_video_with_yt_dlp(video_url: str) -> tuple[str, str]:
 	existing_files = set(os.listdir())
 	ydl.download_with_info_file('$info.json')
 	for file in os.listdir():
@@ -110,7 +135,7 @@ def obtain_video_with_yt_dlp(video_url: str) -> Tuple[str, str]:
 
 MUSIC_EXTS = ['.mp3', '.m4a', '.flac', '.alac', '.wav', '.opus']
 
-def obtain_video_reuse(video_id: str, reuse_dir: os.path) -> Optional[Tuple[str, str]]:
+def obtain_video_reuse(video_id: str, reuse_dir: os.path) -> Optional[tuple[str, str]]:
 	# Try to find a existing file
 	for filepath in os.listdir(reuse_dir):
 		filename = os.path.basename(filepath)
@@ -124,7 +149,7 @@ def obtain_video_reuse(video_id: str, reuse_dir: os.path) -> Optional[Tuple[str,
 			return (mainfile, ext)
 	return None
 
-def obtain_video(video_id: str, video_url: str, reuse_dir: Optional[os.path]) -> Tuple[str, str]:
+def obtain_video(video_id: str, video_url: str, reuse_dir: Optional[os.path]) -> tuple[str, str]:
 	# Use iglob to avoid overhead of collecting into a list, we just want the first item
 	# TODO maybe print a warning if there is more than one $mainfile.*
 	for mainfile in glob.iglob('$mainfile.*'):
@@ -141,41 +166,40 @@ def obtain_video(video_id: str, video_url: str, reuse_dir: Optional[os.path]) ->
 	return obtain_video_with_yt_dlp(video_url)
 
 # Pass 4
-def pass_download_yt_dlp(input_struct, reuse_dir: Optional[os.path]):
-	for entry in input_struct['chunk_list']:
-		# TODO is there a way to skip redownloading the info json?
+def pass_download_yt_dlp(input_struct: InputStruct, reuse_dir: Optional[os.path]):
+	for job in input_struct.job_list:
 		# TODO maybe there is a way to make yt-dlp download to a folder directly instead of this jank mess of changing cwd
 
 		# At this point, we have the proper ID obtained from yt-dlp
 		# But keep consistent with previous code here just in case our ID extraction logic is buggy and got a different one than theirs
-		video_id = entry['video_id']
-		video_url = entry['url']
+		video_id = job.video_id
+		video_url = job.url
 		os.chdir(video_id)
 		mainfile, ext = obtain_video(video_id, video_url, reuse_dir)
-		input_struct['mainfile'] = mainfile
-		input_struct['mainfile_ext'] = ext
+		input_struct.mainfile = mainfile
+		input_struct.mainfile_ext = ext
 		os.chdir('..')
 
 # Pass 6
-def pass_split_chunks(input_struct, output_dir: Optional[os.path]):
-	for entry in input_struct['chunk_list']:
+def pass_split_chunks(input_struct: InputStruct, output_dir: Optional[os.path]):
+	for job in input_struct.job_list:
 		# Assume cwd is already in work_dir, so just video_id is enough
-		output_prefix = output_dir if output_dir is not None else entry['video_id']
-		audio_filepath = os.path.join(entry['video_id'], input_struct['mainfile'])
+		output_prefix = output_dir if output_dir is not None else job.video_id
+		audio_filepath = os.path.join(job.video_id, input_struct.mainfile)
 
-		for chunk in entry['chunks']:
-			chunk_filepath = os.path.join(output_prefix, chunk['out_basename'] + input_struct['mainfile_ext'])
+		for chunk in job.chunks:
+			chunk_filepath = os.path.join(output_prefix, chunk.out_basename + input_struct.mainfile_ext)
 			# Save absolute file path for later passes
-			chunk['out_filepath'] = os.path.abspath(chunk_filepath)
+			chunk.out_filepath = os.path.abspath(chunk_filepath)
 
-			begin_time = chunk['begin_time']
-			end_time = chunk['end_time']
+			begin_time = chunk.begin_time
+			end_time = chunk.end_time
 			if end_time == 'VIDEOLENGTH' and begin_time == 0:
 				# This is a whole video chunk
 				shutil.copyfile(audio_filepath, chunk_filepath)
 				continue
 			elif end_time == 'VIDEOLENGTH':
-				end_time = entry['video_info']['video_length']
+				end_time = job.video_info['duration']
 
 			# open a file, from `ss`, for duration `t`
 			stream = ffmpeg.input(audio_filepath, ss=begin_time, t=(end_time - begin_time))
@@ -188,24 +212,23 @@ def pass_split_chunks(input_struct, output_dir: Optional[os.path]):
 			ffmpeg.run(stream)
 
 # Pass 7
-def pass_apply_tag_ops(input_struct):
-	for entry in input_struct['chunk_list']:
-		for idx, chunk in enumerate(entry['chunks']):
-			chunk_file_path = chunk['out_filepath']
-			f = music_tag.load_file(chunk_file_path)
+def pass_apply_tag_ops(input_struct: InputStruct):
+	for job in input_struct.job_list:
+		for idx, chunk in enumerate(job.chunks):
+			f = music_tag.load_file(chunk.out_filepath)
 
-			for op in itertools.chain(entry['tag_ops'], chunk['tag_ops']):
-				name = op['name']
-				value = op['value']
+			for op in itertools.chain(job.tag_ops, chunk.tag_ops):
+				name = op.name
+				value = op.value
 				if value == '$INDEX':
 					value = idx + 1
 				elif value == '$FILENAME':
-					value = chunk['out_basename'] + input_struct['mainfile_ext']
+					value = chunk.out_basename + input_struct.mainfile_ext
 				elif value == '$FILENAME_ORIG':
 					print('-- [WARN] $FILENAME_ORIG is deprecated, use $CHUNK_NAME instead')
-					value = chunk['chunk_name']
+					value = chunk.chunk_name
 				elif value == '$CHUNK_NAME':
-					value = chunk['chunk_name']
+					value = chunk.chunk_name
 				f[name] = value
 
 			f.save()
@@ -237,16 +260,14 @@ TAG_OP_PREFIX = {
 	'S': 3, #Section
 }
 
-def parse_input_file(file):
-	input_struct = {}
-	chunk_list = input_struct['chunk_list'] = []
+def parse_input_file(f) -> InputStruct:
+	input_struct = InputStruct()
+	job_list = input_struct.job_list
 
-	def make_default_entry():
-		return {'url': '', 'chunks': [], 'tag_ops': []}
-	curr_entry = make_default_entry()
+	curr_job = Job()
 	curr_section_beg = 0
 
-	for line in file:
+	for line in f:
 		# Skip empty lines
 		if not line:
 			continue
@@ -261,60 +282,57 @@ def parse_input_file(file):
 			line = line[1:]
 
 		if url_str := parse_line_with_prefix(line, "url: "):
-			curr_entry['url'] = url_str.strip()
+			curr_job.url = url_str.strip()
 		elif chunk_str := parse_line_with_prefix(line, 'chunk: '):
 			tp = chunk_str.strip().split(" ", 2)
 			if len(tp) != 3:
 				raise RuntimeError('chunk string need exactly 3 components: BEGIN_TIME, END_TIME, CHUNK_NAME separated by spaces')
-			chunks = curr_entry['chunks']
-			chunks.append({
-				'begin_time': MU.strparse_hms_to_seconds(tp[0]),
-				'end_time': parse_unresolved_time(tp[1]),
-				'chunk_name': parse_value(tp[2]),
-				'tag_ops': []
-			})
+			chunks = curr_job.chunks
+			chunks.append(Chunk(
+				begin_time=MU.strparse_hms_to_seconds(tp[0]),
+				end_time=parse_unresolved_time(tp[1]),
+				chunk_name=parse_value(tp[2])))
 		elif line.startswith('chunk_by_chapter: true'):
-			curr_entry['chunk_by_chapter'] = True
+			curr_job.chunk_by_chapter = True
 		elif ext_str := parse_line_with_prefix(line, 'use_file_extension: '):
 			print('-- [WARN] Directive use_file_extension is now deprecated. Chunks file extension will be automatically extracted from the video info.')
-			#curr_entry['use_file_extension'] = parse_value(ext_str)
+			#curr_job.use_file_extension = parse_value(ext_str)
 		elif ext_str := parse_line_with_prefix(line, 'prefix: '):
-			curr_entry['prefix'] = parse_value(ext_str)
+			curr_job.prefix = parse_value(ext_str)
 		elif ext_str := parse_line_with_prefix(line, 'suffix: '):
-			curr_entry['suffix'] = parse_value(ext_str)
+			curr_job.suffix = parse_value(ext_str)
 		elif line.startswith('$$--SECTION BREAK--$$'):
-			curr_section_beg = len(curr_entry['chunks'])
+			curr_section_beg = len(curr_job.chunks)
 		elif ext_str := parse_line_with_prefix(line, 'tag('):
 			(tag_names, _, ext_str) = ext_str.partition(')')
 			tag_value = parse_value(ext_str.removeprefix(': '))
 
 			tag_lists = []
 			if tag_scope == 1:
-				tag_lists.append(curr_entry['tag_ops'])
+				tag_lists.append(curr_job.tag_ops)
 			elif tag_scope == 2:
-				tag_lists.append(curr_entry['chunks'][-1]['tag_ops'])
+				tag_lists.append(curr_job.chunks[-1].tag_ops)
 			elif tag_scope == 3:
-				for i in range(curr_section_beg, len(curr_entry['chunks'])):
-					tag_lists.append(curr_entry['chunks'][i]['tag_ops'])
+				for i in range(curr_section_beg, len(curr_job.chunks)):
+					tag_lists.append(curr_job.chunks[i].tag_ops)
 			else:
 				print('Error: tag scope not specified')
 				sys.exit(-1)
 
 			for tag_name in tag_names.split(','):
 				for tag_list in tag_lists:
-					tag_list.append({
-						'name': tag_name,
-						'value': tag_value
-					})
-		elif line.startswith('---') and curr_entry['url']:
-			# Separator, commit curr_entry
-			chunk_list.append(curr_entry)
+					tag_list.append(TagOp(
+						name=tag_name,
+						value=tag_value))
+		elif line.startswith('---') and curr_job.url:
+			# Separator, commit curr_job
+			job_list.append(curr_job)
 			curr_section_beg = 0
-			curr_entry = make_default_entry()
+			curr_job = Job()
 
-	# Commit last entry if there is one (we assume an entry to exist only if at least a link is present)
-	if curr_entry['url']:
-		chunk_list.append(curr_entry)
+	# Commit last job if there is one (we assume a job to be well-formed only if at least an url is present)
+	if curr_job.url:
+		job_list.append(curr_job)
 
 	return input_struct
 
@@ -351,35 +369,34 @@ prefix: "..."
 def parse_input_file_yaml(file):
 	pass # TODO
 
-def dump_input_struct(input_struct):
+def dump_input_struct(input_struct: InputStruct):
 	# TODO other fields
-	for entry in input_struct['chunk_list']:
-		print(f"url: \"{entry['url']}\"")
-		print(f"chunk_by_chapter: {bool(entry.get('chunk_by_chapter'))}")
-		print(f"prefix: {entry.get('prefix')}")
-		print(f"suffix: {entry.get('suffix')}")
+	for job in input_struct.job_list:
+		print(f"url: \"{job.url}\"")
+		print(f"chunk_by_chapter: {bool(job.chunk_by_chapter)}")
+		print(f"prefix: {job.prefix}")
+		print(f"suffix: {job.suffix}")
 		print('chunks:')
-		for chunk in entry['chunks']:
-			begin_time = chunk['begin_time']
-			end_time = chunk['end_time']
-			def time_fmt(time: str | int) -> str:
+		for chunk in job.chunks:
+			begin_time = chunk.begin_time
+			end_time = chunk.end_time
+			def time_fmt(time: str | float | int) -> str:
 				if isinstance(time, int) or isinstance(time, float):
 					return MU.strformat_seconds(time)
 				else:
 					return time
 			print(f"- begin_time: {time_fmt(begin_time)}")
 			print(f"  end_time: {time_fmt(end_time)}")
-			print(f"  chunk_name: \"{chunk['chunk_name']}\"")
-			print(f"  out_basename: \"{chunk.get('out_basename')}\"")
-			if 'tag_ops' in chunk:
-				print(f"  tag_ops:")
-				for tag_op in chunk['tag_ops']:
-					print(f"  - name: {tag_op['name']}")
-					print(f"    value: \"{tag_op['value']}\"")
+			print(f"  chunk_name: \"{chunk.chunk_name}\"")
+			print(f"  out_basename: \"{chunk.out_basename}\"")
+			print(f"  tag_ops:")
+			for tag_op in chunk.tag_ops:
+				print(f"  - name: {tag_op.name}")
+				print(f"    value: \"{tag_op.value}\"")
 		print('tag_ops:')
-		for tag_op in entry['tag_ops']:
-			print(f"- name: {tag_op['name']}")
-			print(f"  value: \"{tag_op['value']}\"")
+		for tag_op in job.tag_ops:
+			print(f"- name: {tag_op.name}")
+			print(f"  value: \"{tag_op.value}\"")
 
 def clean_outputs(work_dir):
 	for dirpath, dnames, fnames in os.walk(work_dir):
@@ -389,7 +406,7 @@ def clean_outputs(work_dir):
 				print(f'-- Removing {fpath}')
 				os.remove(fpath)
 
-def prompt_continuation(input_struct, question):
+def prompt_continuation(input_struct: InputStruct, question):
 	dump_input_struct(input_struct)
 	if not MU.query_yes_no(question):
 		print('-- Aborting')
