1e519d16065d7cfc244ce93c2ad52e03612bd85f
rtk0c <mail.tianyig@gmail.com>
2024-01-19 17:23:09 -0800

    Improved smart tracknumber tagging

--- id3_autotag.py
+++ id3_autotag.py
@@ -8,20 +8,33 @@ from typing import Optional, Tuple
 
 import my_util as MU
 
-def parse_leading_digits(s: str) -> Tuple[Optional[int], str]:
-    i = 0
-    while i < len(s) and s[i].isdigit():
-        i += 1
-    if i > 0:
-        return (int(s[:i]), s[i:])
-    else:
-        return (None, s)
-
-MUSIC_EXTS = ['mp3', 'm4a', 'flac', 'alac', 'wav', 'opus']
+MUSIC_EXTS = ['mp3', 'm4a', 'flac', 'alac', 'wav', 'ogg', 'opus']
 MUSIC_EXT_STRIPPER = MU.file_ext_stripper(set(MUSIC_EXTS))
-LEADING_DOT_STRIPPER = re.compile(r'^[\. ]*')
+# We want to match the following:
+#     'no. 1' 'No. 1' 'no 1' '1.' '1-' '#1' '#1.' 'no. 1-'
+# but not the following (bare numbers)
+#     '1' '842'
+#
+# The regex divides into three capture groups.
+# 1st: ([Nn]o\.? ?|#)? matches the prefix optionally
+# 2nd: (\d+) matches the number itself
+# 3rd has two parts:
+#     ?(1)(?:\.|-|:)? optionally matches the trailing punctuation if there is a prefix (1st capture group is non-empty)
+#     (?:\.|-|:) mandatorily matches the trailing punctuation otherwise
+# Notice the \.|-|: common to both that describes the trailing punctuation
+#
+# Whole thing is wrapped in word boundary, beacuse matching 'ohno 1' makes no sense
+# We match an optional extra space at the end to substitute out the extra space surrounding the index
+TRACK_NUMBER_PATTERN = re.compile(r'\b([Nn]o\.? ?|#)?(\d+)(?(1)(?:\.|-|:)?|(?:\.|-|:)) ?\b')
+
+def parse_track_number(s: str) -> Optional[int]:
+    res = TRACK_NUMBER_PATTERN.search(s)
+    if res:
+        return int(res.group(2))
+    return None
+
 def parse_track_name(s: str) -> str:
-    return MUSIC_EXT_STRIPPER(LEADING_DOT_STRIPPER.sub('', s))
+    return MUSIC_EXT_STRIPPER(TRACK_NUMBER_PATTERN.sub('', s, count=1))
 
 parser = argparse.ArgumentParser(prog='id3_autotag.py')
 parser.add_argument('-D', '--dry-run', action='store_true')
@@ -52,9 +65,9 @@ for filepath in music_files:
 
     if args.smart_index:
         filename = os.path.basename(filepath)
-        index, rest = parse_leading_digits(filename)
+        index = parse_track_number(filename)
         if index is not None:
-            track_name = parse_track_name(rest)
+            track_name = parse_track_name(filename)
             my_print(f"{filepath}: Assigning track number {index}, name '{track_name}'")
             f['tracknumber'] = index
             f['tracktitle'] = track_name
