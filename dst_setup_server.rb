f7453162dc9b269ff0f06a12aeaa9e79e2586317
rtk0c <mail.tianyig@gmail.com>
2024-02-16 23:50:45 -0800

    Add support for leveldataoverride.lua & examples

--- dst_setup_server.rb
+++ dst_setup_server.rb
@@ -62,10 +62,19 @@ mod_list = Set.new
 conf_shards.each do |shard|
   mods = shard["mods"]
   next if mods.nil?
+
+  # Validate individual mods
   raise RuntimeError unless mods.is_a? Hash
   mods.each do |mod_name, options|
+    # Validate mod name
     raise RuntimeError unless mod_name.is_a?(String) && !mod_name.empty?
+
+    # Validate options
+    if options.nil?
+      options = mods[mod_name] = {}
+    end
     raise RuntimeError unless options.is_a? Hash
+
     mod_name.for_prefix("workshop-") { |id| mod_list.add(id) }
   end
 end
@@ -164,31 +173,55 @@ File.open(File.join(dst_dir, "mods", "dedicated_server_mods_setup.lua"), "w") do
   end
 end
 
+def get_worldgenoverride_lua(world)
+  # TODO support overrides
+  return %{
+return {
+  override_enabled = true,
+  worldgen_preset = "#{world["worldgen_preset"]}",
+  settings_preset = "#{world["settings_preset"]}",
+  overrides = {
+  },
+}
+}
+end
+
 base_port = 10998
 base_port_steam_master = 28900
 base_port_steam_auth = 28700
 
 conf_shards.each_with_index do |shard, i|
-  FileUtils.mkdir_p(File.join(opts[:output], shard["name"]))
+  shard_name = shard["name"]
+  world = shard["world"]
+  mods = shard["mods"]
 
-  File.open(File.join(opts[:output], shard["name"], "worldgenoverride.lua"), "w") do |f|
-    f.puts %{
-return {
-  override_enabled = true,
-  worldgen_preset = "#{shard["worldgen_preset"]}",
-  settings_preset = "#{shard["settings_preset"]}",
-  overrides = {
-  },
-}}
+  FileUtils.mkdir_p(File.join(opts[:output], shard_name))
+
+  case world["setup"]
+  when "worldgenoverride.lua"
+    wgo_path = File.join(opts[:output], shard_name, "worldgenoverride.lua")
+    f = File.open(wgo_path, "w")
+    f.write get_worldgenoverride_lua(world)
+    f.close
+  when "leveldataoverride.lua"
+    ldo_path = File.join(opts[:output], shard_name, "levedataoverride.lua")
+    if world["file"].is_a? String
+      FileUtils.cp(world["file"], ldo_path)
+    elsif world["content"].is_a? String
+      f = File.open(ldo_path, "w")
+      f.write world["content"]
+      f.close
+    else
+      raise RuntimeError, "Selected leveldataoverride.lua but no data is specified"
+    end
   end
 
-  mods = shard["mods"]
-  File.open(File.join(opts[:output], shard["name"], "modoverrides.lua"), "w") do |f|
+  File.open(File.join(opts[:output], shard_name, "modoverrides.lua"), "w") do |f|
     f.puts "return {"
     mods.each do |mod_name, options|
       f.puts "[\"#{mod_name}\"] = {"
       f.puts "enabled = true,"
-      f.puts "configuration_options = #{dump_as_lua(options)}" unless options.empty?
+      f.puts "configuration_options = #{dump_as_lua(options)}"
       f.puts "},"
     end
     f.puts "}"
@@ -196,7 +229,7 @@ return {
   # We could omit the file too if mods.empty?, but that can be confusing as to "why is that file not generating!?"
   # emitting an empty file tells the user more clearly that "you did something wrong, the mods block had nothing in it"
 
-  File.open(File.join(opts[:output], shard["name"], "server.ini"), "w") do |f|
+  File.open(File.join(opts[:output], shard_name, "server.ini"), "w") do |f|
     # Optional config value
     opt = ->(name, def_value=nil) { write_defaulted_val(f, shard, name, def_value) }
     # Required config value
@@ -204,7 +237,7 @@ return {
 
     f.puts "[SHARD]"
     f.puts "is_master = true" if i == 0
-    f.puts "name = #{shard["name"]}"
+    f.puts "name = #{shard_name}"
     if shard.key? "id"
       f.puts "id = #{shard["id"]}"
     else
