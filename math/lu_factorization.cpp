e8f52a531d38927158f1d0c1f88919b28b6d848e
rtk0c <mail.tianyig@gmail.com>
2024-02-24 21:29:09 -0800

    Add fraction support to lu_factorization.cpp

--- math/lu_factorization.cpp
+++ math/lu_factorization.cpp
@@ -15,24 +15,143 @@
 #include <iostream>
 #include <sstream>
 
+int trailingZeros(int n) {
+	//printf("trailingZeros(%d)\n", n);
+	for (int i = 0; i < 32; ++i) {
+		char bit = (n >> i) & 1;
+		if (bit != 0)
+			return i;
+	}
+	return 32;
+}
+
+// It's late, I don't want to think, so copy pasta we go!
+// https://en.wikipedia.org/wiki/Binary_GCD_algorithm
+int calcGcd(int x, int y) {
+	if (x < 0) x = -x;
+	if (y < 0) y = -y;
+
+	//printf("calcGcd(%d, %d)\n", x, y);
+	if (x == 0) return y;
+	if (y == 0) return x;
+
+	int i = trailingZeros(x); x >>= i;
+	int j = trailingZeros(y); y >>= j;
+	int k = std::min(i, j);
+
+	while (true) {
+		assert(x % 2 == 1);
+		assert(y % 2 == 1);
+
+		if (x > y)
+			std::swap(x, y);
+		y -= x;
+		if (y == 0)
+			return x << k;
+		y >>= trailingZeros(y);
+	}
+}
+
+struct Real {
+	int num, denom;
+
+	Real(int a, int b) : num{a}, denom{b} {}
+	Real(int a) : num{a}, denom{1} {}
+	Real() : num{0}, denom{1} {}
+
+	Real(double v) {
+		num = static_cast<int>(v);
+		denom = 1;
+		assert(num == v);
+	}
+
+	void simplify() {
+		int gcd = calcGcd(num, denom);
+		num /= gcd;
+		denom /= gcd;
+		// For -x/-y => x/y
+		// For x/-y => -x/y
+		if (denom < 0) {
+			num = -num;
+			denom = -denom;
+		}
+	}
+};
+
+Real& operator+=(Real& a, Real b) {
+	// I know, this is stupid. Whatever.
+	int n = a.num * b.denom + b.num * a.denom;
+	int d = a.denom * b.denom;
+	a.num = n;
+	a.denom = d;
+	a.simplify();
+	return a;
+}
+Real operator+(Real a, Real b) { return a += b; }
+
+Real& operator-=(Real& a, Real b) {
+	// I know, this is stupid. Whatever.
+	int n = a.num * b.denom - b.num * a.denom;
+	int d = a.denom * b.denom;
+	a.num = n;
+	a.denom = d;
+	a.simplify();
+	return a;
+}
+Real operator-(Real a, Real b) { return a -= b; }
+
+Real& operator*=(Real& a, Real b) {
+	a.num = a.num * b.num;
+	a.denom = a.denom * b.denom;
+	a.simplify();
+	return a;
+}
+Real operator*(Real a, Real b) { return a *= b; }
+
+Real& operator/=(Real& a, Real b) {
+	int n = a.num * b.denom;
+	int d = a.denom * b.num;
+	a.num = n;
+	a.denom = d;
+	a.simplify();
+	return a;
+}
+Real operator/(Real a, Real b) { return a /= b; }
+
+bool operator==(Real a, Real b) {
+	// Assume they are simplified because that's what the functions in this file already do. Too lazy to re-check.
+	return a.num == b.num && a.denom == b.denom;
+}
+bool operator==(Real a, int n) {
+	return a.denom == 1 && a.num == n;
+}
+
+std::ostream& operator<<(std::ostream& s, Real n) {
+	if (n.denom == 1)
+		s << n.num;
+	else
+		s << n.num << '/' << n.denom;
+	return s;
+}
+
 struct Matrix {
 	// Height and width, respectively
 	int rows, cols;
-	std::unique_ptr<double[]> elms;
+	std::unique_ptr<Real[]> elms;
 
 	Matrix(int rows, int cols)
 		: rows{rows}, cols{cols}
-		, elms{std::make_unique<double[]>(rows * cols)}
+		, elms{std::make_unique<Real[]>(rows * cols)}
 	{
-		std::memset(reinterpret_cast<void*>(elms.get()), 0, rows * cols);
+		// elms[] are called with default constructor, no need for extra initialization
 	}
 
-	double& operator[](int row, int col) { return elms[row * cols + col]; }
-	double operator[](int row, int col) const { return elms[row * cols + col]; }
+	Real& operator[](int row, int col) { return elms[row * cols + col]; }
+	Real operator[](int row, int col) const { return elms[row * cols + col]; }
 
 	Matrix copy() const {
 		Matrix res(rows, cols);
-		std::memcpy(reinterpret_cast<void*>(res.elms.get()), reinterpret_cast<void*>(elms.get()), rows * cols * sizeof(double));
+		std::memcpy(reinterpret_cast<void*>(res.elms.get()), reinterpret_cast<void*>(elms.get()), rows * cols * sizeof(Real));
 		return res;
 	}
 
@@ -48,9 +167,9 @@ struct Matrix {
 	void print() const {
 		for (int row = 0; row < rows; ++row) {
 			for (int col = 0; col < cols; ++col) {
-				std::printf("%lf ", operator[](row, col));
+				std::cout << operator[](row, col) << ' ';
 			}
-			putchar('\n');
+			std::cout << '\n';
 		}
 	}
 };
@@ -92,7 +211,7 @@ Matrix parseMatrix(const char* str) {
 			continue;
 		}
 
-		mat[row, col] = v;
+		mat[row, col] = Real(v);
 		++col;
 	}
 	return mat;
@@ -111,7 +230,8 @@ void gaussianEliminationToREF(Matrix& U, Matrix& L) {
 
 	// n: row and column index along the main diagonal
 	for (int n = 0; n < U.rows; ++n) {
-		if (U[n,n] == 0.0) {
+		// TODO is this even the correct logic?
+		if (U[n,n] == 0) {
 			// Non-linear dependent column
 			continue;
 		}
@@ -124,14 +244,13 @@ void gaussianEliminationToREF(Matrix& U, Matrix& L) {
 		++L_col;
 		//putchar('\n');
 
-		double factor = 1/U[n,n];
+		Real factor = 1/U[n,n];
 		for (int col = n; col < U.cols; ++col)
 			U[n,col] *= factor;
 
-
 		// Reduce the rest of the rows
 		for (int r2 = n + 1; r2 < U.rows; ++r2) {
-			double f = U[r2,n] / U[n,n];
+			Real f = U[r2,n] / U[n,n];
 			for (int col = n; col < U.cols; ++col)
 				U[r2,col] -= U[n,col] * f;
 		}
@@ -141,8 +260,8 @@ void gaussianEliminationToREF(Matrix& U, Matrix& L) {
 // Make L have only 1's along the diagonal, moving the information into U instead
 void simplifyLU(Matrix& L, Matrix& U) {
 	for (int i = 0; i < L.cols; ++i) {
-		double fu = L[i,i];
-		double fl = 1/L[i,i];
+		Real fu = L[i,i];
+		Real fl = 1/L[i,i];
 		for (int j = i; j < L.rows; ++j)
 			L[j,i] *= fl;
 		for (int j = i; j < U.cols; ++j)
