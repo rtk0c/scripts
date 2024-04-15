4a39df7a97b7761974055b0c7f084d6f10d12453
rtk0c <mail.tianyig@gmail.com>
2024-04-14 17:59:06 -0700

    Fix reference to non-existent `read_confirm`

--- fish-ddop/dclean_all.fish
+++ fish-ddop/dclean_all.fish
@@ -1,9 +1,13 @@
 function dclean_all
 	set trash_path "/tmp/$USER-wksp-trash"
 	echo "This is going to delete everything in $trash_path"
-	if read_confirm
-		rm -rf -v $trash_path
-	else
-		echo "Bailing"
+	read -l -P 'Do you want to continue? [y/N] ' confirm
+	switch $confirm
+		case Y y
+			rm -rf -v $trash_path
+			return 0
+		case '' N n
+			echo "Bailing"
+			return 1
 	end
 end
