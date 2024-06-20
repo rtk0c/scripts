cbcec97d55fae8a952a15a72e588fcddd78935f8
rtk0c <mail.tianyig@gmail.com>
2024-06-20 15:33:24 +0800

    Add script for viewing DST server logs in tmux

---
+++ dstds/dstserv_simple_logs_in_tmux.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# NOTE: these must have no spaces in them
+SESSION='DST-servers'
+DST_SERVER_NAME='testworld'
+
+tmux new-session -d -s $SESSION
+
+tmux new-window -t $SESSION:1 -n 'DST'
+tmux send-keys -t $SESSION:1 'cd ~/Games/DontStarveTogether/' Enter "./start.sh $DST_SERVER_NAME"
+
+tmux new-window -t $SESSION:2 -n 'DST logs'
+tmux split-window -h -t $SESSION:2
+tmux split-window -v
+tmux select-pane -t 0
+tmux split-window -v
+
+tmux select-pane -t 0
+tmux send-keys -t $SESSION:2 "cd ~/Games/DontStarveTogether/$DST_SERVER_NAME" Enter "tail -f Master/server_log.txt" Enter
+tmux select-pane -t 2
+tmux send-keys -t $SESSION:2 "cd ~/Games/DontStarveTogether/$DST_SERVER_NAME" Enter "tail -f Caves/server_log.txt" Enter
+tmux select-pane -t 1
+tmux send-keys -t $SESSION:2 "cd ~/Games/DontStarveTogether/$DST_SERVER_NAME" Enter "tail -f Island/server_log.txt" Enter
+tmux select-pane -t 3
+tmux send-keys -t $SESSION:2 "cd ~/Games/DontStarveTogether/$DST_SERVER_NAME" Enter "tail -f Volcano/server_log.txt" Enter
