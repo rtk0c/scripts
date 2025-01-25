23bb950736e91930b1f4ffa5ea7bf0e0370655ca
rtk0c <mail.tianyig@gmail.com>
2025-01-25 12:09:50 -0800

    txt2md.pl

--- ebook/txt2md.pl
+++ ebook/txt2md.pl
@@ -2,40 +2,41 @@
 use strict;
 use warnings;
 
-if (!defined $1 || !defined $2) {
+if (!defined $ARGV[0] || !defined $ARGV[1]) {
 	die "Usage: txt2md.pl <input.txt> <output.md>";
 }
 
-open my f_in, "<", $1 or die "Cannot open input file: $!";
-open my f_out, ">", $2 or die "Cannot open output file: $!";
+open my $f_in, "<", $ARGV[0] or die "Cannot open input file: $!";
+open my $f_out, ">", $ARGV[1] or die "Cannot open output file: $!";
 
-my NUMERAL = qr/[0-9一二三四五六七八九十零百千万〇]/;
+my $NUMERAL = qr/[0-9一二三四五六七八九十零百千万〇]/;
 
 # 第一卷 卷一 etc.
-my DELIM_1 = qr/[第卷部]/;
+my $DELIM_1 = qr/[第卷部]/;
 # 第一章 章一 etc.
-my DELIM_2 = qr/[第章集节]/;
+my $DELIM_2 = qr/[第章集节]/;
 
-my HEADING_1 = /^(${DELIM_1}?${NUMERAL}*${DELIM_1}?)/;
-my HEADING_2 = /^(${DELIM_2}?${NUMERAL}*${DELIM_2}?)/;
+my $HEADING_1 = qr/^(${DELIM_1}${NUMERAL}*${DELIM_1}?)/;
+my $HEADING_2 = qr/^(${DELIM_2}${NUMERAL}*${DELIM_2}?)/;
 
-my $prev_line
-while (my $line = <f_in>) {
-	if ($line =~ $HEADING_1) {
-		$line = "# " . $line;
-	} elsif ($line =~ $HEADING_2) {
-		$line = "## " . $line;
-	}
-
+my $prev_line = "";
+while (my $line = <$f_in>) {
 	# Insert a blank line after every paragraph lines
-	# 4 spaces or 2 "fullwidth space" (U+3000 IDEOGRAPHIC SPACE)
-	if ($line != '\n' && ($prev_line =~ m/^ {4}/ || $prev_line =~ m/^　{2}/)) {
-		$line = "\n" . $line
+	# = 4 spaces or 2 "fullwidth space" (U+3000 IDEOGRAPHIC SPACE)
+	if ($line ne "\n" && ($prev_line =~ /^(?: {4}|　{2})/)) {
+		print $f_out "\n";
 	}
 
-	print f_out $line;
-	$prev_line = $line
+	if ($line =~ $HEADING_1) {
+		print $f_out ("# " . $line);
+	} elsif ($line =~ $HEADING_2) {
+		print $f_out ("## " . $line);
+	} else {
+		print $f_out $line;
+	}
+
+	$prev_line = $line;
 }
 
-close f_in;
-close f_out;
+close $f_in;
+close $f_out;
