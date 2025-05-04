175aad74b02843925312f08b80444ed6376d6300
rtk0c <mail.tianyig@gmail.com>
2025-05-04 16:58:11 -0700

    Revert back to manual route manip
    - Installing https://github.com/dlenski/vpn-slice is too much work
    - Made less jank by removing the onexit handler
      - routes associated with an interface are automatically removed on
        interface removal

--- openconnect-sso/sjsu.sh
+++ openconnect-sso/sjsu.sh
@@ -16,15 +16,28 @@ else
 	echo "cookie: $COOKIE"
 fi
 
-EXTRA_ARGS=()
 
 # https://stackoverflow.com/a/1885534
-read -p "Route 10.0.0.0/8 only, instead of accepting the AnyConnect gateway's advertised routes? (y/N)" REPLY
+read -p "Replacing the default everything route with 10.0.0.0/8 only route? (y/N)" REPLY
 echo #Move to next line
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	EXTRA_ARGS+=('--script')
-	EXTRA_ARGS+=('vpn-slice 10.0.0.0/8')
+	ROUTE_LAN_ONLY=true
 fi
 
 # The gateway advertises 10.0.0.0/8 AND a default route, which we don't care about
-echo $COOKIE | sudo openconnect "$HOST" --cookie-on-stdin --servercert "$FINGERPRINT" "${EXTRA_ARGS[@]}"
+echo $COOKIE | sudo openconnect "$HOST" --cookie-on-stdin --servercert $FINGERPRINT &
+
+# OpenConnect is running as background, it takes a bit to connect and setup its routes.
+# Jank, I know. This is what "cloud" and all the docker clusters do apparently.
+sleep 5s
+
+# TODO replace this with a proper vpnc-script to setup the correct routes in the first go
+if [[ $ROUTE_LAN_ONLY = true ]]; then
+	# replace VPN's routes
+	sudo ip route del default dev tun0 && sudo ip route add 10.0.0.0/8 dev tun0
+	# disable using VPN's DNS globally
+	sudo resolvectl default-route tun0 false
+fi
+
+wait
+
