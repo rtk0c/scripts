aa0c3932552769230b3a23af834573f3b9307220
rtk0c <mail.tianyig@gmail.com>
2025-05-24 19:56:07 -0700

    Add vpn-slice support, again; add better interactions

--- openconnect-sso/sjsu.sh
+++ openconnect-sso/sjsu.sh
@@ -4,40 +4,47 @@
 : "${HOST:=https://vpn.sjsu.edu/}"
 echo "server: $HOST"
 
+: "${VPN_ROUTE:=10.0.0.0/8}"
+
 if [[ $FINGERPRINT = '' ]]; then
-	read -p "enter server fingerprint: " FINGERPRINT
+	echo 'set $FINGERPRINT to avoid this interactive prompt'
+	echo 'note: you may past the whole line from `openconnect-sso --authenticate`, like FINGER=xxxxyyyyzzzz'
+	read -p 'enter server fingerprint: ' FINGERPRINT
+	FINGERPRINT=${FINGERPRINT#'FINGERPRINT='}
 else
 	echo "fingerprint: $FINGERPRINT"
 fi
 
 if [[ $COOKIE = '' ]]; then
-	read -p "enter cookie: " COOKIE
+	echo 'set $COOKIE to avoid this interactive prompt'
+	read -p 'enter cookie: ' COOKIE
+	COOKIE=${COOKIE#'COOKIE='}
 else
 	echo "cookie: $COOKIE"
 fi
 
+if [[ $(type -P vpn-slice) ]]; then
+	# vpn-slice exists, use that
 
-# https://stackoverflow.com/a/1885534
-read -p "Replacing the default everything route with 10.0.0.0/8 only route? (y/N)" REPLY
-echo #Move to next line
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	ROUTE_LAN_ONLY=true
-fi
+	echo $COOKIE | sudo openconnect "$HOST" \
+		--cookie-on-stdin --servercert $FINGERPRINT \
+		-s "vpn-slice $VPN_ROUTE"
+else
+	# use manual hack
 
-# The gateway advertises 10.0.0.0/8 AND a default route, which we don't care about
-echo $COOKIE | sudo openconnect "$HOST" --cookie-on-stdin --servercert $FINGERPRINT &
+	echo $COOKIE | sudo openconnect "$HOST" \
+		--cookie-on-stdin --servercert $FINGERPRINT \
+		&
 
-# OpenConnect is running as background, it takes a bit to connect and setup its routes.
-# Jank, I know. This is what "cloud" and all the docker clusters do apparently.
-sleep 5s
+	# OpenConnect is running as background, it takes a bit to connect and setup its routes.
+	# Jank, I know. This is what "cloud" and all the docker clusters do apparently.
+	sleep 5s
 
-# TODO replace this with a proper vpnc-script to setup the correct routes in the first go
-if [[ $ROUTE_LAN_ONLY = true ]]; then
-	# replace VPN's routes
-	sudo ip route del default dev tun0 && sudo ip route add 10.0.0.0/8 dev tun0
+	echo 'removing default route'
+	sudo ip route del default dev tun0
+	sudo ip route add 10.0.0.0/8 dev tun0
 	# disable using VPN's DNS globally
 	sudo resolvectl default-route tun0 false
+
+	wait
 fi
-
-wait
-
